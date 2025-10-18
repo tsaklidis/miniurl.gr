e6eee3adc121735e476088ca40f52cecd64e39ad
Stefanos I. Tsaklidis <stefanos@tsaklidis.gr>
2025-10-18 17:05:17 +0300

    Do not expose Users model

--- app/admin/admin.py
+++ app/admin/admin.py
@@ -6,5 +6,5 @@ from app.databases.models import Urls, User
 class UrlsAdmin(ModelView, model=Urls):
     column_list = [Urls.alias, Urls.original_url, Urls.created_at, Urls.total_clicks]
 
-class UserAdmin(ModelView, model=User):
-    column_list = [User.username, User.email, User.disabled, User.is_admin]
+# class UserAdmin(ModelView, model=User):
+#     column_list = [User.username, User.email, User.disabled, User.is_admin]
