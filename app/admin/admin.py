77a35298c8fd4fff5e12bf08f0028b0222eb435f
Stefanos I. Tsaklidis <stefanos@tsaklidis.gr>
2025-10-21 21:39:17 +0300

    Reverse order for admin panel

--- app/admin/admin.py
+++ app/admin/admin.py
@@ -5,6 +5,7 @@ from app.databases.models import Urls, User
 
 class UrlsAdmin(ModelView, model=Urls):
     column_list = [Urls.alias, Urls.original_url, Urls.created_at, Urls.total_clicks]
+    column_default_sort = ("created_at", True)
 
 # class UserAdmin(ModelView, model=User):
 #     column_list = [User.username, User.email, User.disabled, User.is_admin]
