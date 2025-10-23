7a9efb8a9064f69a480d9b4531cb225f7aa10e00
Stefanos I. Tsaklidis <stefanos@tsaklidis.gr>
2025-10-23 21:54:44 +0300

    Add health tests

--- app/main.py
+++ app/main.py
@@ -30,7 +30,7 @@ app = FastAPI(
         "name": "Stefanos I. Tsaklidis",
         "url": "https://tsaklidis.gr",
     },
-    # docs_url=None, redoc_url=None, openapi_url=None
+    docs_url=None, redoc_url=None, openapi_url=None
 )
 
 # Add exception handler and limiter
